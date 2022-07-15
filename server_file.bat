636a63fbb7a412a76007463dac7d9fad19402e00
Darkinsi <fornaridarian@gmail.com>
2022-07-15 03:51:26 +0200

    Add files via upload

---
+++ server_file.bat
@@ -0,0 +1,17 @@
+@echo off
+
+color 0b
+
+echo start Downloading 
+
+start https://mega.nz/file/Q4gxlBIQ#0GySfI-HxCBgQ0-R1MrieSS2gETME1QccSj0Nv3XPpo
+
+start https://mega.nz/file/E8xk3QjZ#-4fJ2YEVYkSoo_i9xG_4sXhF-CJndnCUqP4Xpl1YS64
+
+start https://mega.nz/file/s8ZlVazQ#zXXCWFruFeNc0Q8ONFbJ7FqYpYqkK1rAgSNl2BSvRos
+
+echo Welcome Home D-K
+
+pause 
+
+close 
